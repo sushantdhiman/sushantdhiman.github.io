cd145d233f28d038d864d0fa1e953424c3c71ac9
sushantdhiman <sushantdhiman@outlook.com>
2020-04-05 12:13:15 +0000

    (build) travis 68

---
+++ _next/static/TdwNvhDOJvzX7VJKw2Qcs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/blog/diminishing-capability":[a],"/blog/parabel-of-onion":[a],"/blog/price":[a]}}("static\u002Fchunks\u002Fca2db0ee6255bd4c3934dcb77f9640dd6d56fb58.b47c2e522878f36635cc.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
